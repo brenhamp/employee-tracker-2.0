4a34c2e9168dc03750fd403f7670fad5096e52b5
brenhamp <bpozzi@trinity.edu>
2022-02-09 17:41:39 -0600

    established express connection in server.js

---
+++ .history/server_20220209173620.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }))
---
+++ .history/server_20220209173630.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json)
