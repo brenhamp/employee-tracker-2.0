5a9d5198c31195a1ac2370c046bc87e2913622ed
brenhamp <bpozzi@trinity.edu>
2022-02-16 04:17:06 -0600

    deleted unnecessary file and folder

--- inquirer/index.js
+++
@@ -1,9 +0,0 @@
-// const app = require('inquirer');
-// const express = require('express');
-// const router = express.Router();
-
-// app.use(require('./inquirer/inquirer'));
-
-// module.exports = router;
-
-const databaseFunction = require ('../index.js')
