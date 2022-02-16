1e19e9e91a8854f8cc93df10f700a2ffe8470013
brenhamp <bpozzi@trinity.edu>
2022-02-16 04:53:14 -0600

    removed some commented-out code

--- .history/server_20220216045218.js
+++ .history/server_20220216045218.js
@@ -11,3 +11,7 @@ db.connect(err => {
       console.log(`Server running on port ${PORT}`);
     });
   });
+
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
